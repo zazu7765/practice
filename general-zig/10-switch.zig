48417ef9fbff5f524d261069ebf0d0122890b7e5
zazu7765 <41345110+zazu7765@users.noreply.github.com>
2024-08-05 12:36:22 -0400

    add switch cases

---
+++ general-zig/10-switch.zig
@@ -0,0 +1,24 @@
+const expect = @import("std").testing.expect;
+test "switch case" {
+    var x: i8 = 10;
+    switch (x) {
+        -1...1 => {
+            x = -x;
+        },
+        10, 100 => {
+            x = @divExact(x, 10);
+        },
+        else => {},
+    }
+    try expect(x == 1);
+}
+
+test "switch as expression" {
+    var x: i8 = 10;
+    x = switch (x) {
+        -1...1 => -x,
+        10, 100 => @divExact(x, 10),
+        else => x,
+    };
+    try expect(x == 1);
+}
